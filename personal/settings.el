6bf15d8089db1baaf9d1901f73bdf3f10e01abaf
Jordan Davis <3215984+jordantdavis@users.noreply.github.com>
2026-03-12 20:50:34 -0500

    Move settings from personal/preload/ to personal/. - Correctly load afternoon theme - Set line numbers to display relatively - Set line length limit to 100

--- personal/settings.el
+++ personal/settings.el
@@ -1,8 +1,12 @@
 ;;; settings.el --- Personal settings -*- lexical-binding: t; -*-
 
-(setq prelude-theme 'afternoon)
+(load-theme 'afternoon t)
 
 (set-face-attribute 'default nil
                     :family "Fira Code"
                     :height 140
                     :weight 'normal)
+
+(setq display-line-numbers-type 'relative)
+
+(setq whitespace-line-column 100)
